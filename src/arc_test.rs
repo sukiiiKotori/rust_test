7d1a08baa74a3a20c396fedeacbe967389f22b3e
琴梨梨 <11704182+pyy_fyy@user.noreply.gitee.com>
2023-10-09 23:00:06 +0800

    修改测试用例

--- src/arc_test.rs
+++ src/arc_test.rs
@@ -9,6 +9,7 @@ fn get_string(re: ???) -> String {
 fn get_strong_count(re: ???) -> usize {
     //请完成这个函数，作用是返回该智能指针中的强引用计数值
     //返回值类型已给出，请自行写出函数的形参
+    //注意，请谨慎思考所需的形参类型，使得该Arc智能指针 （不会！！！）因为函数调用而增加引用计数
     todo!();
 }
 
@@ -20,11 +21,23 @@ mod tests {
     #[test]
     fn testcase2() {
         let sp_arc = Arc::new("hello world".to_string());
-        let sp2 = Arc::clone(&sp_arc);
+        
         let a = get_string(&sp_arc);
-
         assert_eq!(a, "hello world".to_string());
+
+        assert_eq!(get_strong_count(&sp_arc), 1);
+
+        let sp2 = Arc::clone(&sp_arc);
         assert_eq!(get_strong_count(&sp_arc), 2);
+
+        let sp3 = Arc::clone(&sp_arc);
+        assert_eq!(get_strong_count(&sp_arc), 3);
+
+        drop(sp3);
+        assert_eq!(get_strong_count(&sp_arc), 2);
+
+        drop(sp2);
+        assert_eq!(get_strong_count(&sp_arc), 1);
 
     }
 }
