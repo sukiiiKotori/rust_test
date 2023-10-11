248dd04ca0d355ea8c22ec96df003a76278490aa
琴梨梨 <11704182+pyy_fyy@user.noreply.gitee.com>
2023-10-12 00:04:58 +0800

    格式化源码

--- transport_tool.py
+++ transport_tool.py
@@ -1,35 +1,36 @@
 import socket
 
-name='test111'#把这里改成你自己的姓名
+name = 'test111'  # 把这里改成你自己的姓名
 
 def CreatCilent():
-    tcp_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    tcp_socket.connect(('192.168.1.116',6666))
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.connect(('192.168.1.116', 6666))
     tcp_socket.send((name).encode('utf-8'))
     while True:
         while True:
-            path=input('''please input file path such as 'xxx_test.rs': ''')
+            path = input('''please input file path such as 'xxx_test.rs': ''')
             try:
-                f=open('src/' + path,'rb') 
+                f = open('src/' + path, 'rb')
                 break
             except IOError:
                 print('file path ERROR')
-        #转为字节流
-        data=f.read()
+        # 转为字节流
+        data = f.read()
         f.close()
-        #计算长度
-        lenth=len(data)
-        #发送长度
-        lenth_bytes=lenth.to_bytes(length=4,byteorder='big',signed=False)
+        # 计算长度
+        lenth = len(data)
+        # 发送长度
+        lenth_bytes = lenth.to_bytes(length=4, byteorder='big', signed=False)
         tcp_socket.send(lenth_bytes)
-        #发送文件名
-        fname=path
+        # 发送文件名
+        fname = path
         print(fname)
         tcp_socket.send((fname+'\n').encode('ascii'))
-        #发送文件流
-        print("sending file size:%dB"%len(data))
+        # 发送文件流
+        print("sending file size:%dB" % len(data))
         tcp_socket.send(data)
         print(fname+" is all sent!")
+
 
 if __name__ == '__main__':
     CreatCilent()
